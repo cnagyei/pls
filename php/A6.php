b4c6f1b0c084c71d8b5cf6ae59b3053cb9485d83
cnagyei <cnagyei15@gmail.com>
2024-06-21 12:44:35 +0000

    update assignment

---
+++ php/A6.php
@@ -0,0 +1,15 @@
+<?php
+// INPUT
+$e = 1;
+$t = 2;
+$d = 3;
+$t = 4;
+$i = 5;
+$v = 6;
+
+// PROCESS
+$d = (($e ** (-$t * -$t)) * $d * $t) / ($t - ($i * $v));
+
+// OUTPUT
+echo "Output: d = $d";
+?>
---
+++ php/A7.php
@@ -0,0 +1,11 @@
+<?php
+// INPUT
+$a = 1;
+$b = 2;
+
+// PROCESS
+$c = sqrt(($a ** 2) + ($b ** 2)) + sqrt(($a ** 3) + ($b ** 3));
+
+// OUTPUT
+echo "Output: c = $c";
+?>
